2ff9307ef7e89b1d225b7296b95aae5a0bfb9998
mark <mark.jeongh@gmc-labs.com>
2021-06-30 12:29:49 +0900

    feat: first commit

---
+++ src/main/java/com/gmc/cicdgithubactionsample/CiCdGithubActionSampleApplication.java
@@ -0,0 +1,13 @@
+package com.gmc.cicdgithubactionsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdGithubActionSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdGithubActionSampleApplication.class, args);
+    }
+
+}
